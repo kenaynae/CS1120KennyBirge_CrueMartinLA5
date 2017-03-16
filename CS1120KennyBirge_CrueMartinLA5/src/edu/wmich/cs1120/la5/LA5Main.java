4288e6e3397ad1c91757eae7cd8a0bb6b825e441
Kenny Birge <Kenny Birge@10.80.141.13>
2017-03-16 02:05:36 -0400

    Added/Built path for .jar and the 2 files. Also added Main

--- CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/LA5Main.java
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -1,10 +1,11 @@
 package edu.wmich.cs1120.la5;
 
+import edu.wmich.cs1120.la5.gui.MainWindow;
+
 public class LA5Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainWindow.startGUI();
 	}
 
 }
