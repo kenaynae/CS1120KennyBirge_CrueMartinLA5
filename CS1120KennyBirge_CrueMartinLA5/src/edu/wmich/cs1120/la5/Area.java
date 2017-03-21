52a44d80311f3e4c93f0cc5edb0d665b861b5d16
Crue <Crue@10.80.231.24>
2017-03-21 19:33:10 -0400

    Added methods to Area class and created HighArea and LowArea classes
    HighArea and LowArea extend Area

--- CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/Area.java
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/Area.java
@@ -1,30 +1,33 @@
 package edu.wmich.cs1120.la5;
 
-public abstract class Area implements IArea{
-	private double basicEnergyCost;
-	pricate double radiation;
-	private double 
+public  class Area implements IArea{
+	protected double basicEnergyCost;
+	private double radiation;
+	private double elevation;
 	
 	public double getBasicEnergyCost(){
+		return this.basicEnergyCost;
 	
 	}
 	public void setBasicEnergyCost(double basicEnergyCost){
+		this.basicEnergyCost=basicEnergyCost;
 	
 	}
 	public double getElevation(){
-	
+		return this.elevation;
 	}
 	public void setElevation(double elevation){
-	
+		this.elevation= elevation;
 	}
 	public double getRadiation(){
-	
+		return this.radiation;
 	}
 	public void setRadiation(double radiation){
+		this.radiation= radiation;
 	
 	}
 	public double calcConsumedEnergy(){
-	
+		return 0;
 	}
 	
 
