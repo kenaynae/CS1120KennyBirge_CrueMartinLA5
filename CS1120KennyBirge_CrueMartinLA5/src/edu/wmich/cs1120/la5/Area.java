9482597a10692f83be2b56253d819a393b6073fc
Crue <Crue@10.80.231.24>
2017-03-21 19:22:48 -0400

    Created Area class that implements IArea

---
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/Area.java
@@ -0,0 +1,31 @@
+package edu.wmich.cs1120.la5;
+
+public abstract class Area implements IArea{
+	private double basicEnergyCost;
+	pricate double radiation;
+	private double 
+	
+	public double getBasicEnergyCost(){
+	
+	}
+	public void setBasicEnergyCost(double basicEnergyCost){
+	
+	}
+	public double getElevation(){
+	
+	}
+	public void setElevation(double elevation){
+	
+	}
+	public double getRadiation(){
+	
+	}
+	public void setRadiation(double radiation){
+	
+	}
+	public double calcConsumedEnergy(){
+	
+	}
+	
+
+}
