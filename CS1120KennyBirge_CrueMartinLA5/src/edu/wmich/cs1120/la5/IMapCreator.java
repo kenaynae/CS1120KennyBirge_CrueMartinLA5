c8b7fb8d220903e53712bd4e70a1c9579291d769
Crue <Crue@10.80.148.50>
2017-03-22 10:53:12 -0400

    Edited IMapCreator

--- CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/IMapCreator.java
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/IMapCreator.java
@@ -4,4 +4,7 @@ import java.io.IOException;
 
 public interface IMapCreator {
 	void scanTerrain(String fileName, int threshold) throws IOException;
+	public TerrainScanner getScanner();
+	public void setScanner(TerrainScanner scanner);
+	
 }
