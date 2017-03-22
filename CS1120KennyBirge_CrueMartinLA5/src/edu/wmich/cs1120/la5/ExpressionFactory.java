499ab97a358c454dd22e23ab3bd7d14841a7d013
Kenny Birge <Kenny Birge@10.80.225.113>
2017-03-22 12:01:22 -0400

    added ExpressionFactory

--- CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
@@ -2,6 +2,14 @@ package edu.wmich.cs1120.la5;
 
 public class ExpressionFactory {
 	public static IExpression getExpression(char operator, int val1, int val2) {
-		
+		IOperation op;
+		IExpression exp1 = new Literal(val1);
+		IExpression exp2 = new Literal(val2);
+		if (operator == '+')
+			op = new Addition();
+		else
+			op = new Subtraction();
+		IExpression exp = new BinaryExpression(exp1, exp2, op);
+		return exp;
 	}
 }
