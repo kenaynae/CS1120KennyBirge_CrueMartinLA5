95750e8e26157d9a6e420d95df6b3c0647648ef5
Kenny Birge <Kenny Birge@10.80.225.113>
2017-03-22 11:25:35 -0400

    added RadiationAnalyzer and EnergyAnalyzer

--- CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
@@ -2,7 +2,7 @@ package edu.wmich.cs1120.la5;
 
 import java.util.ArrayList;
 
-public class EnergyAnalyzer implements IRover {
+public class RadiationAnalyzer implements IRover {
 	private String analysis;
 	private ArrayList<IArea> path;
 	private IMapCreator mc;
@@ -20,11 +20,11 @@ public class EnergyAnalyzer implements IRover {
 		this.path = path;
 	}
 	public void analyzePath() {
-		double energy = 0.0;
+		double radiation = 0.0;
 		for (int i = 0; i<path.size()-1; i++) {
-			energy+=path.get(i).calcConsumedEnergy();
+			radiation+=path.get(i).getRadiation();
 		}
-		analysis = energy + "";
+		analysis = radiation + "";
 	}
 	public String getAnalysis() {
 		return analysis;
