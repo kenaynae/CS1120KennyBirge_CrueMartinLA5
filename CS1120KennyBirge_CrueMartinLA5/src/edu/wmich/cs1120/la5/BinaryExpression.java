c856e3bd1e7cc4d6f18c680128ef6a186640de2d
Crue <Crue@10.80.148.50>
2017-03-22 11:54:55 -0400

    Created Literal and BinaryExpression classes

---
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -0,0 +1,18 @@
+package edu.wmich.cs1120.la5;
+
+public class BinaryExpression implements IExpression{
+	private IOperation op;
+	private IExpression left;
+	private IExpression right;
+	
+	public BinaryExpression(IExpression left, IExpression right, IOperation op){
+		this.left =left;
+		this.right =right;
+		this.op = op;
+	}
+	
+	public Integer getValue(){
+		return op.perform(this.left, this.right);
+	}
+
+}
---
+++ CS1120KennyBirge_CrueMartinLA5/src/edu/wmich/cs1120/la5/Literal.java
@@ -0,0 +1,13 @@
+package edu.wmich.cs1120.la5;
+
+public class Literal implements IExpression{
+	private int value;
+	
+	public Literal(int value){
+		this.value= value;
+	}
+	
+	public Integer getValue(){
+		return value;
+	}
+}
